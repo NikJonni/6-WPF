4b67db6ff3ba09aa99f9796d9ae0475d3c672a75
Никита Егоров <nik.egorov1994@mail.ru>
2022-02-24 19:37:54 +0300

    Добавьте файлы проекта.

---
+++ 6 WPF/WeatherControl.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _6_WPF
+{
+    internal class WeatherControl : DependencyObject
+    {
+        private string wind_direction;
+        private int wind_speed;
+        private enum Precipitation
+        {
+            sunny,
+            cloudy,
+            rain,
+            snow
+        }
+        public string WindDirecction { get; set; }
+        public int WindSpeed { get; set; }
+        public WeatherControl(string winddir, int windsp)
+        {
+            this.WindDirecction = winddir;
+            this.WindSpeed = windsp;
+        }
+        public static readonly DependencyProperty TempProperty;
+        public int Temp
+        {
+            get => (int)GetValue(TempProperty);
+            set => SetValue(TempProperty, value);
+        }
+        static WeatherControl()
+        {
+            TempProperty = DependencyProperty.Register(
+                nameof(Temp),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsRender,
+                    null,
+                    new CoerceValueCallback(CoerceTemp)),
+                new ValidateValueCallback(ValidateTemp));
+        }
+        private static bool ValidateTemp(object value)
+        {
+            int v = (int)value;
+            if (v >= -50 && v <= 50)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private static object CoerceTemp(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50 && v <= 50)
+            {
+                return v;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
